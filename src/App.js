37bbabfcb5ab84ee8c225fe617ab32ec2940cde3
David Terry <me@xwvvvvwx.com>
2019-04-11 03:29:26 +0200

    add forms

--- src/App.js
+++ src/App.js
@@ -3,24 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        proposal: '',
+        content: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form>
+        <label>
+          content:
+          <input
+            name="content"
+            type="text"
+            value={this.state.content}
+            onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          proposal:
+          <input
+            name="proposal"
+            type="text"
+            value={this.state.proposal}
+            onChange={this.handleChange} />
+        </label>
+      </form>
     );
   }
 }
