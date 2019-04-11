1a1016de7bad43e15db489ba48bee64da370c595
David Terry <me@xwvvvvwx.com>
2019-04-11 14:27:02 +0200

    use react-hooks, add abi

--- src/App.js
+++ src/App.js
@@ -1,92 +1,66 @@
-import React, { Component } from 'react';
-import { useWeb3Context } from 'web3-react'
-import IPFS from 'ipfs-mini';
-import './App.css';
+import React, { useState } from "react";
+import { useWeb3Context } from "web3-react";
+import IPFS from "ipfs-mini";
+import Cms from "./abis/Cms.json";
+import "./App.css";
 
 const ipfs = new IPFS({
-  host: 'ipfs.infura.io',
+  host: "ipfs.infura.io",
   port: 5001,
-  protocol: 'https'
+  protocol: "https"
 });
 
-let context = null;
-const Web3Setter = props => {
-  if (context === null) {
-    context = useWeb3Context();
-    context.setConnector('metaMask');
+const addToIpfs = async content => {
+  try {
+    const hash = await ipfs.add(content);
+    console.log("content added successfully!");
+    console.log(`https://ipfs.infura.io/ipfs/${hash}`);
+    return hash;
+  } catch (err) {
+    console.error(`failed to add content ${content} to ipfs`, err);
   }
-  console.log(context);
+}
 
-  return <div />;
-};
+const App = () => {
+  const [proposal, setProposal] = useState('');
+  const [content, setContent] = useState('');
 
-class App extends Component {
-  state = {
-    proposal: '',
-    content: ''
-  };
-
-  handleChange = ({ target }) => {
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit = async event => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    try {
-      const hash = await ipfs.add(this.state.content);
-      console.log('content added successfully!');
-      console.log(`https://ipfs.infura.io/ipfs/${hash}`);
-    } catch (err) {
-      console.error(`failed to add content ${this.state.content} to ipfs`, err);
-    }
-
-  };
-
-  render() {
-    return (
-	  <>
-
-	   <div hidden>
-	     <Web3Setter />
-	   </div>
-
-       <div className="App">
-         <form onSubmit={this.handleSubmit}>
-           <label>
-             content
-             <textarea
-               name="content"
-               type="text"
-               value={this.state.content}
-               onChange={this.handleChange}
-             />
-           </label>
-
-           <br />
-
-           <label>
-             proposal
-             <input
-               name="proposal"
-               type="text"
-               value={this.state.proposal}
-               onChange={this.handleChange}
-             />
-           </label>
-
-           <br />
-
-           <input type="submit" value="Submit" />
-         </form>
-       </div>
-	  </>
-    );
+    const hash = await addToIpfs(content);
   }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          content
+          <textarea
+            name="content"
+            type="text"
+            value={content}
+			onChange={e => setContent(e.target.value)}
+          />
+        </label>
+
+        <br />
+
+        <label>
+          proposal
+          <input
+            name="proposal"
+            type="text"
+            value={proposal}
+			onChange={e => setProposal(e.target.value)}
+          />
+        </label>
+
+        <br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Web3Provider, { Connectors } from 'web3-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Web3Provider, { Connectors } from "web3-react";
 
 const metaMask = new Connectors.MetaMaskConnector({ supportedNetworks: 1 });
 
 ReactDOM.render(
-  <Web3Provider connectors={{"metaMask": metaMask}}>
+  <Web3Provider connectors={{ metaMask }}>
     <App />
   </Web3Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
